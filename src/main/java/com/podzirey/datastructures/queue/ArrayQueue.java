6c38e81fc2cbce932cdff6568fa046eef58eb75e
Eugene Podzirey <e.podzirey@gmail.com>
2021-11-25 22:43:29 +0200

    Initial Commit

---
+++ src/main/java/com/podzirey/datastructures/queue/ArrayQueue.java
@@ -0,0 +1,89 @@
+package com.podzirey.datastructures.queue;
+
+public class ArrayQueue implements Queue{
+    private int size;
+    private Object[] array;
+
+    public ArrayQueue(){
+        array = new Object[7];
+    }
+
+    public ArrayQueue(int initialCapacity){
+        array = new Object[initialCapacity];
+    }
+
+    @Override
+    public void enqueue(Object value) {
+        ensureCapacity();
+        array[size] = value;
+        size++;
+    }
+
+    private void ensureCapacity(){
+        if(array.length == size){
+            Object[] newArray = new Object[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+    }
+
+    @Override
+    public Object dequeue() {
+        if(isEmpty()){
+            throw new IllegalStateException("Queue is Empty!");
+        }
+        Object result = array[0];
+        Object[] newArray = new Object[array.length - 1];
+        for (int i = 0; i < size - 1; i++) {
+            newArray[i] = array[i + 1];
+        }
+        array = newArray;
+        size--;
+        return result;
+    }
+
+
+    @Override
+    public Object peek() {
+        return array[0];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        for (int i = 0; i < size; i++) {
+            Object valueInQueue = array[i];
+            if(value.equals(valueInQueue)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public String toString(){
+        String result = "[" ;
+        for (int i = 0; i < size; i++) {
+            String temp = String.valueOf(array[i]);
+            result = result + temp;
+        }
+
+        return result + "]";
+    }
+}
---
+++ src/test/java/com/podzirey/datastructures/queue/ArrayQueueTest.java
@@ -0,0 +1,102 @@
+package com.podzirey.datastructures.queue;
+
+import com.podzirey.datastructures.queue.ArrayQueue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayQueueTest {
+
+    @Test
+    public void testEnqueueAndDequeueWorkCorrectlyAndChangeSize(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("Q");
+        arrayQueue.enqueue("W");
+
+        assertEquals(2, arrayQueue.size());
+        assertEquals("Q", arrayQueue.dequeue());
+        assertEquals("W", arrayQueue.dequeue());
+        assertEquals(0, arrayQueue.size());
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testEnqueueAndDequeueAndPeek(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("Q");
+        arrayQueue.enqueue("W");
+        arrayQueue.enqueue("E");
+
+        assertEquals("Q", arrayQueue.peek());
+        assertEquals("Q", arrayQueue.dequeue());
+
+        assertEquals("W", arrayQueue.peek());
+        assertEquals("W", arrayQueue.dequeue());
+
+        assertEquals(1, arrayQueue.size());
+
+        assertFalse(arrayQueue.isEmpty());
+
+        assertEquals("E", arrayQueue.peek());
+        assertEquals("E", arrayQueue.dequeue());
+
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyReturnTrueOnQueueAfterClear(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("Q");
+        arrayQueue.enqueue("W");
+
+        arrayQueue.clear();
+
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testContainsReturnTrue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("Q");
+
+        assertTrue(arrayQueue.contains("Q"));
+
+    }
+
+    @Test
+    public void testContainsReturnFalse(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("Q");
+
+        assertFalse(arrayQueue.contains("J"));
+
+    }
+
+    @Test
+    public void testContainsReturnFalseOnEmptyQueue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+
+        assertFalse(arrayQueue.contains("SHO"));
+    }
+
+    @Test
+    public void testToStringWorksCorrectlyOnFullQueue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("Q");
+        arrayQueue.enqueue("W");
+        arrayQueue.enqueue("E");
+
+        assertEquals("[QWE]", arrayQueue.toString());
+
+    }
+
+    @Test
+    public void testThrowIllegalStateExceptionWhenDequeueOnEmptyQueue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            arrayQueue.dequeue();
+        });
+    }
+
+}
